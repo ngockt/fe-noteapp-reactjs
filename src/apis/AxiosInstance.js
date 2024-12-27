2d0ba2bab07f9fa467ff5bbb24ec00d8839f4379
Ngoc Khuc <ngoc.khuc@eastgate-software.com>
2024-12-27 10:36:08 +0700

    update locahost

--- src/apis/AxiosInstance.js
+++ src/apis/AxiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const AxiosInstance = axios.create({
-    baseURL: 'http://10.147.19.45:8000', // Set your base URL here
+    baseURL: 'http://localhost:8000', // Set your base URL here
     headers: {
         'Content-Type': 'application/json',
     },
